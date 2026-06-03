024164e21f41a8e6f275c2a80ffec777e723388a
Jeffrin Romero <jeffrinromerom@gmail.com>
2026-06-03 10:33:19 -0600

    logos modificados

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         background_color: '#003034',
         icons: [
           {
-            src: 'logo.png',
+            src: 'logoSinFondo.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'logo.png',
+            src: 'logoSinFondo.png',
             sizes: '512x512',
             type: 'image/png'
           }
